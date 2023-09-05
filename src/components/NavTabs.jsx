7dfa13297a335b1a880fbb33a686c1170d68df1d
cfinn7789 <cfinn7789@gmail.com>
2023-09-05 16:56:54 -0400

    added functionality to form

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -4,7 +4,7 @@ function NavTabs() {
   const currentPage = useLocation().pathname;
 
   return (
-    <ul className="nav nav-tabs">
+    <ul className="nav nav-tabs justify-content-end">
       <li className="nav-item">
         <Link
           to="/"
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,72 @@
+import React, { useState } from 'react';
+
 export default function Contact() {
-    return (
-      <div>
-        <h1>Name</h1>
-        <input class="input is-primary" type="text" placeholder="Name"></input>
-        <h1>Email</h1>
-        <input class="input is-primary" type="text" placeholder="Email"></input>
-        <h1>Message</h1>
-        <input class="input is-primary" type="text" placeholder="Message"></input>
-      </div>
-    );
-  }
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = () => {
+    if (formData.name.trim() === '' || formData.email.trim() === '' || formData.message.trim() === '') {
+      alert('Please fill in all fields.');
+    } else {
+      setFormSubmitted(true);
+      setTimeout(() => {
+        setFormData({
+          name: '',
+          email: '',
+          message: '',
+        });
+        setFormSubmitted(false);
+      }, 3000);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Name</h1>
+      <input
+        className="input is-primary"
+        type="text"
+        placeholder="Name"
+        name="name"
+        value={formData.name}
+        onChange={handleInputChange}
+      />
+      <h1>Email</h1>
+      <input
+        className="input is-primary"
+        type="text"
+        placeholder="Email"
+        name="email"
+        value={formData.email}
+        onChange={handleInputChange}
+      />
+      <h1>Message</h1>
+      <input
+        className="input is-primary"
+        type="text"
+        placeholder="Message"
+        name="message"
+        value={formData.message}
+        onChange={handleInputChange}
+      />
+      <button type="button" className="btn btn-success" onClick={handleSubmit}>
+        Submit
+      </button>
+
+      {formSubmitted && <p className="success-message">Form submitted successfully!</p>}
+    </div>
+  );
+}
