35a5f7c132928c6e7d776cc716ec378f2a883b2e
cfinn7789 <cfinn7789@gmail.com>
2023-09-20 14:27:55 -0400

    added image

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,12 @@
+import me from '../../images/Snapchat-1705161328.jpg';
+
 export default function About() {
     return (
       <div>
-        <h1 className="title">About Me</h1>
+        <h1 className="title">Chelsey Finn</h1>
+        <figure class="image is-128x128">
+          <img src={me}></img>
+        </figure>
         <p>
           Hello. I'm Chelsey, a 23-year old full-stack developer. Welcome to my page!
           Currently I'm a student at UCF's coding bootcamp.
